998258d8d1841a11c58a3aa00799f3a455fba866
Jan Schulz-Hofen <jan@plan.io>
2012-07-12 12:30:31 +0200

    Revert "Adding more columns:"
    This reverts commit 41dfdb4a083b8e0532c828282d145ac2d45c83d9.

--- init.rb
+++ init.rb
@@ -2,7 +2,6 @@ require 'redmine'
 require 'dispatcher'
 
 Dispatcher.to_prepare do
-  Issue.send(:include, RedmineSpentTimeColumn::Patches::IssuePatch) unless Issue.include?(RedmineSpentTimeColumn::Patches::IssuePatch)
   Query.send(:include, RedmineSpentTimeColumn::Patches::QueryPatch) unless Query.include?(RedmineSpentTimeColumn::Patches::QueryPatch)
   QueriesHelper.send(:include, RedmineSpentTimeColumn::Patches::QueriesHelperPatch) unless QueriesHelper.include?(RedmineSpentTimeColumn::Patches::QueriesHelperPatch)
 end
@@ -11,5 +10,5 @@ Redmine::Plugin.register :redmine_spent_time_column do
   name 'Redmine Spent Time Column'
   author 'Jan Schulz-Hofen, Planio GmbH'
   description 'This plugin adds a spent time column to issue lists.'
-  version '1.1.0'
+  version '1.0.0'
 end
